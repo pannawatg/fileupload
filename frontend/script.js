481e427c8da06e160c2e67529c679dd271cca037
pannawatg <nathapol971@gmail.com>
2026-08-04 18:51:50 +0700

    Refactor file upload logic and update status handling

--- frontend/script.js
+++ frontend/script.js
@@ -1,32 +1,21 @@
-const URL = "laterbro"
-const uploadForm = document.getElementById('uploadForm');
-const inputFile = document.getElementById('inputFile');
-const statusText = document.getElementById('status');
+const fileInput = document.getElementById("fileInput");
+const uploadButton = document.getElementById("UploadButton");
+const statusElement = document.getElementById("status");
 
-uploadForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
+function updateStatus(msg){
+    statusElement.textContent = msg;
+}
 
-  const file = inputFile.files[0];
-  if (!file) return;
-
-  statusText.textContent = 'Uploading to backend...';
-
-  const formData = new FormData();
-  formData.append('file', file);
-  try {
-    const response = await fetch(`${URL}/upload`, {
-      method: 'POST',
-      body: formData,
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      statusText.innerHTML = `Uploaded! <a href="${data.githubUrl}" target="_blank">View on GitHub</a>`;
-    } else {
-      statusText.textContent = `Error: ${data.error}`;
+uploadButton.addEventListener('click', function(){
+    const file = fileInput.files[0];
+    if(!file){
+        updateStatus("Please select a file.");
+        return;
     }
-  } catch (err) {
-    statusText.textContent = 'Error connecting to backend.';
-  }
+    updateStatus(
+        `Success!
+        Name: ${file.name}
+        Size: ${(file.size / 1024 / 1024).toFixed(3)} MB
+        Type: ${file.type || "Unknown"}`
+    );
 });
